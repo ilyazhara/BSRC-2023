3aeeeb47e248d52c7cfcfc1e758faea5e5b17af7
Valentin Buchnev <buchnev.valentin@gmail.com>
2023-07-06 20:56:00 +0300

    fix datasets info

--- bnn_competition/dataloaders/datasets_info.py
+++ bnn_competition/dataloaders/datasets_info.py
@@ -1,11 +1,10 @@
-# TODO(Valya): simplify paths to datasets
 DATASETS_PATHS = {
-    "div2k_train": "/mnt/disk2-part1/datasets_pytorch/superresolution/DIV2K/train",
-    "div2k_val": "/mnt/disk2-part1/datasets_pytorch/superresolution/DIV2K/validation",
-    "set5": "/mnt/disk2-part1/datasets_pytorch/superresolution/benchmark/Set5",
-    "set14": "/mnt/disk2-part1/datasets_pytorch/superresolution/benchmark/Set14",
-    "b100": "/mnt/disk2-part1/datasets_pytorch/superresolution/benchmark/B100",
-    "urban100": "/mnt/disk2-part1/datasets_pytorch/superresolution/benchmark/Urban100",
+    "div2k_train": "/path/to/DIV2K/train",
+    "div2k_val": "/path/to/DIV2K/validation",
+    "set5": "/path/to/benchmark/Set5",
+    "set14": "/path/to/benchmark/Set14",
+    "b100": "/path/to/benchmark/B100",
+    "urban100": "/path/to/benchmark/Urban100",
 }
 
 AVAILABLE_BENCHMARKS = ["set5", "set14", "b100", "urban100"]
